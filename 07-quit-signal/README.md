64abaaf3d5976a649a7a370166a02d2032b78510
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-11-27 22:54:20 +0800

    update

--- 07-quit-signal/README.md
+++ 07-quit-signal/README.md
@@ -1,3 +1,29 @@
 ```mermaid
-
+sequenceDiagram
+    participant M as Main
+    participant B as Baker Goroutine
+    participant C as Cake Channel
+    participant Q as Quit Channel
+    
+    Note over M,B: Setup Phase
+    M->>B: Create baker goroutine
+    B->>C: Create cake channel
+    
+    Note over M,B: Baking Phase
+    rect rgb(240, 240, 240)
+        loop 5 times
+            B->>C: Send cake ready message
+            C->>M: Receive cake message
+            Note over B: Random sleep (200-1000ms)
+        end
+    end
+    
+    Note over M,B: Cleanup Phase
+    M->>Q: Send "Stop baking"
+    Q->>B: Receive quit signal
+    B->>Q: Send cleanup confirmation
+    Q->>M: Receive final message
+    
+    Note over B: Goroutine exits
+    Note over C: Channel closes
 ```
--- 07-quit-signal/main.go
+++ 07-quit-signal/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func baker(name string, quit chan string, r *rand.Rand) <-chan string {
+	c := make(chan string)
+	go func() {
+		defer close(c)
+		for i := 1; ; i++ {
+			select {
+			case c <- fmt.Sprintf("%s: Cake #%d is ready!", name, i):
+			case message := <-quit:
+				fmt.Printf("[%s] Stopping work: %s. Cleaning up...\n", name, message)
+				quit <- fmt.Sprintf("%s: All baking equipment is cleaned up. Goodbye!", name)
+				return
+			}
+			time.Sleep(time.Duration(r.Intn(800)+200) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+func main() {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	quit := make(chan string)
+	bakery := baker("Chef Paul", quit, r)
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(<-bakery)
+	}
+
+	quit <- "Stop baking"
+	fmt.Println("Final Message:", <-quit)
 }
